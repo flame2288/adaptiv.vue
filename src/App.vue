c0339aa5da929f9bf03b04653889bbe955c99710
flame2288 <ay-860@mail.ru>
2024-04-20 16:44:03 +0500

    aleshapopovich

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-<script setup lang="ts">
-import VHeader from './components/VHeader.vue';
-import VFooter from './components/VFooter.vue';
-import VPromo from './components/VPromo.vue';
-import VProjects from './components/VProjects.vue';
-import VTech from './components/VTech.vue';
-import VFeelback from './components/VFeelback.vue';
+<script setup>
+import VHeader from '@/components/VHeader.vue'
+import VFooter from '@/components/VFooter.vue'
+import VPromo from '@/components/VPromo.vue'
+import VProjects from '@/components/VProjects.vue'
+import VTech from '@/components/VTech.vue'
+import VFeelback from '@/components/VFeelback.vue'
 </script>
 
 <template>
